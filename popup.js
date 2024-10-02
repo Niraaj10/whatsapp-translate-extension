1ed756018ad89147bced1e32779a9c2a87e8dd61
Niraaj10 <nitcode10@gmail.com>
2024-10-02 19:22:51 +0530

    File Setup

---
+++ popup.js
@@ -0,0 +1,9 @@
+
+document.getElementById("translateChatBtn").addEventListener("click", () => {
+    const inputLang = document.getElementById("inputLang").value;
+    const outputLang = document.getElementById("outputLang").value;
+  
+   
+  });
+  
+  
